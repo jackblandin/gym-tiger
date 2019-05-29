84ab5a5b9c8715449647e2a011862e604d00a39b
Jack Blandin <jblandin@gohealth.com>
2019-05-29 12:41:21 -0500

    Change episode end to max number of steps

--- gym_tiger/envs/tiger_env.py
+++ gym_tiger/envs/tiger_env.py
@@ -10,7 +10,6 @@ import numpy as np
 OBS_GROWL_LEFT = [1, 0, 0, 0]
 OBS_GROWL_RIGHT = [0, 1, 0, 0]
 OBS_START = [0, 0, 1, 0]
-OBS_END = [0, 0, 0, 1]
 
 ACTION_OPEN_LEFT = 0
 ACTION_OPEN_RIGHT = 1
@@ -26,7 +25,7 @@ class TigerEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, reward_tiger=-100, reward_gold=10, reward_listen=-1,
-                 obs_accuracy=.85):
+                 obs_accuracy=.85, max_steps_per_episode=100):
         """
         OpenAI Gym environment for the partially observable Tiger game.
 
@@ -41,11 +40,28 @@ class TigerEnv(gym.Env):
         obs_accuracy : numeric
             Number b/w 0 and 1. The accuracy of the growl. I.e. obs_accuracy of
             1 means that a GROWL_LEFT implies TIGER_LEFT 100% of the time.
+        max_steps_per_episode : int, default=100
+            Maximum allowed steps per episode. This will define how long an
+            episode lasts, since the Tiger game does not end otherwise.
+
+        Attributes
+        ----------
+        curr_episode : int
+            Current episode as a count.
+        action_episode_memory : list<int>
+            History of actions taken in episode.
+        curr_step : int
+            Current timestep in episode, as a count.
+        action_space : gym.spaces.Discrete
+            Action space.
+        observation_space : gym.spaces.Discrete
+            Observation space.
         """
         self.reward_tiger = reward_tiger
         self.reward_gold = reward_gold
         self.reward_listen = reward_listen
         self.obs_accuracy = obs_accuracy
+        self.max_steps_per_episode = max_steps_per_episode
 
         self.__version__ = "0.1.0"
         logging.info("TigerEnv - Version {}".format(self.__version__))
@@ -61,7 +77,7 @@ class TigerEnv(gym.Env):
         # Define what the agent can do: LISTEN, OPEN_LEFT, OPEN_RIGHT
         self.action_space = spaces.Discrete(3)
 
-        # Define what agent can observe: GROWL_LEFT, GROWL_RIGHT, START, END
+        # Define what agent can observe: GROWL_LEFT, GROWL_RIGHT, START
         self.observation_space = spaces.Discrete(4)
 
     def step(self, action):
@@ -95,15 +111,17 @@ class TigerEnv(gym.Env):
                 However, official evaluations of your agent are not allowed to
                 use this for learning.
         """
-        done = self.left_door_open or self.right_door_open
+        done = self.curr_step >= self.max_steps_per_episode
         if done:
             raise RuntimeError("Episode is done")
         self.curr_step += 1
         self._take_action(action)
         # Recompute done since action may have modified it
-        done = self.left_door_open or self.right_door_open
+        done = self.curr_step >= self.max_steps_per_episode
         reward = self._get_reward()
         ob = self._get_obs()
+        # Perform resets that happen after each timestep
+        self._step_reset()
         return ob, reward, done, {}
 
     def reset(self):
@@ -120,7 +138,6 @@ class TigerEnv(gym.Env):
         self.action_episode_memory.append([])
         self.left_door_open = False
         self.right_door_open = False
-        # TODO fix this random update
         self.tiger_left = np.random.randint(0, 2)
         self.tiger_right = 1 - self.tiger_left
         return OBS_START
@@ -151,8 +168,6 @@ class TigerEnv(gym.Env):
             return 'GROWL_RIGHT'
         elif obs[2] == 1:
             return 'START'
-        elif obs[3] == 1:
-            return 'END'
         else:
             raise ValueError('Invalid observation: '.format(obs))
 
@@ -229,7 +244,12 @@ class TigerEnv(gym.Env):
         """
         last_action = self.action_episode_memory[self.curr_episode][-1]
         if last_action != ACTION_LISTEN:
-            return OBS_END
+            # Return accurate observation, but this won't be informative, since
+            # the tiger will be reset afterwards.
+            if self.tiger_left:
+                return OBS_GROWL_LEFT
+            else:
+                return OBS_GROWL_RIGHT
         # Return accurate observation
         if np.random.rand() < self.obs_accuracy:
             if self.tiger_left:
@@ -242,3 +262,18 @@ class TigerEnv(gym.Env):
                 return OBS_GROWL_RIGHT
             else:
                 return OBS_GROWL_LEFT
+
+    def _step_reset(self):
+        """
+        Performs resets that happen after each timestep.
+
+        Returns
+        -------
+        None
+        """
+        # Make sure doors are closed
+        self.left_door_open = False
+        self.right_door_open = False
+        self.tiger_left = np.random.randint(0, 2)
+        self.tiger_right = 1 - self.tiger_left
+
